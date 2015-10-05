e692c588b603a4477512f52906a919c7dbf18388
Bo Chean <bchean@gmail.com>
2015-10-05 11:44:48 +0100

    Use Heroku's PORT config var.

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
     mongoUri: process.env.MONGO_URI,
-    expressPort: process.env.EXPRESS_PORT,
+    expressPort: process.env.PORT || 80,
     youtubeApiKey: process.env.YOUTUBE_API_TOKEN
 };
