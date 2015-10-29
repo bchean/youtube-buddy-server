12197ccf2b59571ff32519f40795c59910a71d82
Bo Chean <bchean@gmail.com>
2015-10-29 02:48:56 -0700

    Refactor YouTube E2E tests.

--- test/e2e/youtube.js
+++ test/e2e/youtube.js
@@ -3,24 +3,22 @@ var should = require('should'),
 
 describe('youtube [e2e]', function() {
     describe('#getVideoInfo', function() {
-        describe('E2E (live YouTube API)', function() {
-            it('valid YouTube id', function(done) {
-                var youtubeWrapper = new YouTubeWrapper();
-                youtubeWrapper.getVideoInfo('TnYacrJuc7g', function(err, videoInfo) {
-                    should.not.exist(err);
-                    videoInfo.title.should.equal('Test video for YouTube Buddy Server');
-                    videoInfo.lengthSeconds.should.equal(6);
-                    done();
-                });
+        it('valid YouTube id', function(done) {
+            var youtubeWrapper = new YouTubeWrapper();
+            youtubeWrapper.getVideoInfo('TnYacrJuc7g', function(err, videoInfo) {
+                should.not.exist(err);
+                videoInfo.title.should.equal('Test video for YouTube Buddy Server');
+                videoInfo.lengthSeconds.should.equal(6);
+                done();
             });
+        });
 
-            it('invalid YouTube id', function(done) {
-                var youtubeWrapper = new YouTubeWrapper();
-                youtubeWrapper.getVideoInfo('invalid', function(err, videoInfo) {
-                    err.should.equal('No YouTube video exists with id: invalid');
-                    should.not.exist(videoInfo);
-                    done();
-                });
+        it('invalid YouTube id', function(done) {
+            var youtubeWrapper = new YouTubeWrapper();
+            youtubeWrapper.getVideoInfo('invalid', function(err, videoInfo) {
+                err.should.equal('No YouTube video exists with id: invalid');
+                should.not.exist(videoInfo);
+                done();
             });
         });
     });
